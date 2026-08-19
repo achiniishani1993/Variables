1899f0af878042e4920bb9803a0286a65b0410a9
achiniishani1993 <achiniishani1993@gmail.com>
2026-08-19 10:00:47 +0200

    completed 3 tasks

---
+++ homework.js
@@ -0,0 +1,32 @@
+// First task
+
+let firstName = "Achini";
+let favoriteNumber = 7;
+let likesCoding = true;
+
+console.log(`Hi, my name is ${firstName}, my favorite number is ${favoriteNumber}, and it is ${likesCoding} that I like coding.`);
+
+// Extra task
+
+let birthYear = 1993;
+let currentYear = 2026;
+
+// Calculate your age using subtraction. 
+
+let age = currentYear - birthYear; 
+
+console.log(`I am ${age} years old.`);
+
+// Advance task
+
+let food = ["Pizza", "Smashpotato", "Fish"];
+let student = {
+  firstName: firstName,
+  age: age,
+  likesCoding: likesCoding,
+  food:food
+
+};
+
+console.log(student);
+console.log(student.firstName);
